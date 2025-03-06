634abbed45c240bcc975fd230eb120c35de45533
GeonHo Song <songgun1234@gmail.com>
2025-03-06 11:10:30 +0900

    fix : 리액트 오류 해결

--- frontend_repo/src/index.js
+++ frontend_repo/src/index.js
@@ -3,43 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter } from "react-router-dom";
-import Notfound from "./pages/NotFound";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import AllProducts from "./pages/AllProducts";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, path: "/", element: <Home /> },
-      { path: ".products", element: <AllProducts /> },
-      {
-        path: "/products/new",
-        element: (
-          <ProtectedRoute requireAdmin>
-            <NewProduct />
-          </ProtectedRoute>
-        ),
-      },
-      { path: "/products/:id", element: <ProductDetail /> },
-      {
-        path: "/carts",
-        element: (
-          <ProtectedRoute>
-            <MyCart />
-          </ProtectedRoute>
-        ),
-      },
-    ],
-  },
-]);
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
