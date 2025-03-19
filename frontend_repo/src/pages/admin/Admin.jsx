f962ece64db01f15d4dd1d83c3edd30d2774c096
GeonHo Song <songgun1234@gmail.com>
2025-03-19 14:32:21 +0900

    feat: adminButton 제작 및 admin 페이지 컴포넌트 제작

--- frontend_repo/src/pages/admin/Admin.jsx
+++ frontend_repo/src/pages/admin/Admin.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import AdminButton from "../../components/ui/AdminButton";
 export default function Admin() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-4 ml-3">
+      {/*상품코드 작성 div만들어서 작성하세요*/}
+      <AdminButton text={"상품"} className="w-2 h-1" />
+      {/*개별상품 작성 div만들어서 작성하세요*/}
+      <AdminButton text={"개별"} />
+
+      <AdminButton text={"행사"} />
+      <AdminButton text={"지점"} />
+    </div>
+  );
 }
