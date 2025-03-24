5450ac9cae4ee1fca1df38d0ad9ad53fd48ab8bc
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-24 16:49:39 +0900

    feat : 전국/지점별 재고량 productDetails 에서 확인

--- frontend_repo/src/api/httpProductService.js
+++ frontend_repo/src/api/httpProductService.js
@@ -18,3 +18,30 @@ export async function fetchProductFavoriteHome(token) {
   // 응답 데이터에서 실제 상품 배열을 추출
   return response.data;
 }
+
+// productCode 별 상품 totalQuantity 찾기
+export async function fetchtotalQuantityByProductCode(productCode) {
+  console.log("fetchtotalQuantityByProductCode 요청");
+
+  const response = await instance.get(
+    `/inventory/findByProductCode/${productCode}`
+  );
+
+  console.log("fetchtotalQuantityByProductCode.response:", response);
+  return response;
+}
+
+// productCode, branchName 별 상품 quantity 찾기
+export async function fetchQuantityByProductCodeAndBranchName(
+  productCode,
+  branchName
+) {
+  console.log("fetchQuantityByProductCodeAndBranchName 요청");
+
+  const response = await instance.get(
+    `/inventory/findByProductCodeAndBranchName/${productCode}/${branchName}`
+  );
+
+  console.log("fetchQuantityByProductCodeAndBranchName.response:", response);
+  return response;
+}
